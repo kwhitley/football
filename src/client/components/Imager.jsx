46ca1bc345172db6866e94d3d660ac024edd8b0a
Kevin Whitley <krwhitley@gmail.com>
2018-12-30 11:05:36 -0600

    flexbox grid

--- src/client/components/Imager.jsx
+++ src/client/components/Imager.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 export default ({
   src
 }) => {
-  let previewPath = src.replace(/^(.*)\.(\w{3,4})$/, '$1::width=300,height=300.$2')
+  let previewPath = src.replace(/^(.*)\.(\w{3,4})$/, '$1::width=500,height=500,quality=90.$2')
 
   return (
-    <img src={previewPath} width="100%" />
+    <img src={previewPath} />
   )
 }
