de36ef178caca51747a98c0c3fcec496dd5683d1
Kevin Whitley <krwhitley@gmail.com>
2018-12-30 11:15:17 -0600

    responsive grid

--- src/client/components/Imager.jsx
+++ src/client/components/Imager.jsx
@@ -6,6 +6,6 @@ export default ({
   let previewPath = src.replace(/^(.*)\.(\w{3,4})$/, '$1::width=500,height=500,quality=90.$2')
 
   return (
-    <img src={previewPath} />
+    <img src={previewPath} width="100%" />
   )
 }
