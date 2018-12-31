ef6b4cb2391694ce2a797abf68c848015f0127b7
Kevin Whitley <krwhitley@gmail.com>
2018-12-31 11:55:38 -0600

    file structure cleanup and fragments now properly save when served via api

--- src/server/imager-api.js
+++ src/server/imager-api.js
@@ -1,18 +1,22 @@
 import express from 'express'
-import { getImage, getImageWorker } from './imager-worker'
+import { getImage } from './imager'
+import fs from 'fs'
 
 const app = express()
 
 // single route catches all requests to imager and passes them to worker
 app.get('*.(png|jpg)', (req, res) => {
   getImage(req.path)
-    .then((image) => {
+    .then(({ image, path }) => {
       res.set('Content-Type', 'image/jpeg')
       res.set('Cache-Control', "public, max-age=345600") // 4 days
       res.set('Expires', new Date(Date.now() + 345600000).toUTCString())
 
+      let filestream = fs.createWriteStream(path)
+
       image.stream(function (err, stdout, stderr) {
         stdout.pipe(res)
+        stdout.pipe(filestream)
       })
     })
     .catch((err) => {
