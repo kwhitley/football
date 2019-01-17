05dcb78b6c5238a4191ab00bfafa56fb13ff8a4b
Kevin Whitley <krwhitley@gmail.com>
2019-01-17 13:43:35 -0600

    new back button styles, fixed toggle, and toggle doesnt appear if no images loaded

--- src/client/components/AdminToggle.jsx
+++ src/client/components/AdminToggle.jsx
@@ -2,15 +2,17 @@ import React from 'react'
 import { observer, inject } from 'mobx-react'
 import classNames from 'classnames'
 
-export const AdminToggle = ({ app }) =>
-  <div
-    className={classNames('toggle', 'admin', app.adminMode && 'active')}
-    onClick={app.toggleAdmin}
-    >
-    {
-      app.adminMode ? 'editing' : 'locked'
-    }
-  </div>
+export const AdminToggle = ({ app, images }) =>
+  images.items.length
+  ? <div
+      className={classNames('toggle', 'admin', app.adminMode && 'active')}
+      onClick={app.toggleAdmin}
+      >
+      {
+        app.adminMode ? 'editing' : 'locked'
+      }
+    </div>
+  : null
 
-export default inject('app')(observer(AdminToggle))
+export default inject('app', 'images')(observer(AdminToggle))
 
--- src/client/components/Back.jsx
+++ src/client/components/Back.jsx
@@ -2,8 +2,6 @@ import React from 'react'
 
 export const Back = ({ history, location }) =>
   location.pathname !== '/' &&
-  <div className="back" onClick={history.goBack}>
-    Go Back
-  </div>
+  <div className="back" onClick={history.goBack} />
 
 export default Back
