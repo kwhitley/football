33a33cfaef91cf658dc72af18936c45d23906135
Kevin Whitley <krwhitley@gmail.com>
2019-01-22 15:12:06 -0600

    editMode toggle scrolls to bottom to prevent page jump

--- src/client/state/app.js
+++ src/client/state/app.js
@@ -1,10 +1,17 @@
-import { observable, action } from 'mobx'
+import { observable, action, reaction } from 'mobx'
 
 export class AppState {
   @observable editMode = false
 
   @action toggleMode = (which) => this[`${which}Mode`] = !this[`${which}Mode`]
   @action toggleEdit = () => this.toggleMode('edit')
+
+  constructor() {
+    reaction(
+      () => this.editMode,
+      (editing) => setTimeout(() => document.body.scrollTo(0, 99999), 0),
+    )
+  }
 }
 
 export default window.images = new AppState()
