182789454fd9236b8e080f70349c4f093bf72fd2
Kevin Whitley <krwhitley@gmail.com>
2019-02-11 11:28:22 -0600

    added react icons and toggle action for editing

--- src/client/components/Collection/Item/Details.jsx
+++ src/client/components/Collection/Item/Details.jsx
@@ -1,26 +1,117 @@
 import React, { useState, useEffect } from 'react'
 import md from 'react-markings'
+import classNames from 'classnames'
+import { FiEdit, FiEye } from 'react-icons/fi'
 
 import LiveEdit from '../../Controls/LiveEdit'
 import Input from '../../Controls/Input'
 import Inspect from '../../Controls/Inspect'
+import { ActionIcon, ActionIconToggle } from '../../Controls/ActionIcons'
 import { ownsCollection } from '../../../hooks'
 
-export default function ImageDetails({ collectionId, item, updateItemAction }) {
+const useUpdate = ({ item, path }) => {
   let [ update, setUpdate ] = useState(item)
   let [ isDirty, setIsDirty ] = useState(false)
-  let editable = ownsCollection(collectionId)
+
+  useEffect(() => {
+    if (update !== item) {
+      setIsDirty(true)
+    }
+  }, [update])
+
+  return {
+    update, setUpdate, isDirty
+  }
+}
+
+export function Editable({
+  placeholder,
+  className,
+  value,
+  editing = false,
+  onChange,
+  children,
+  ...props,
+}) {
+  // let { update, setUpdate, isDirty } = useUpdate({
+  //   item: value,
+  //   // path: `/api/collections/${collectionId}/items/${item.id}`,
+  // })
+
+  console.log('creating Editable', value)
+  if (!editing) {
+    return children
+  }
+
+  return (
+    <LiveEdit
+      className={className}
+      value={value}
+      onChange={onChange}
+      placeholder={placeholder}
+      />
+  )
+}
+
+export default function ImageDetails({ collectionId, item, updateItemAction }) {
+  let { update, setUpdate, isDirty } = useUpdate({
+    item,
+    path: `/api/collections/${collectionId}/items/${item.id}`,
+  })
+  let [ preview, setPreview ] = useState('false')
+  const togglePreview = () => setPreview(!preview)
+
+  // let [ update, setUpdate ] = useState(item)
+  let isOwner = ownsCollection(collectionId)
+  let editable = !preview && isOwner
 
   return (
     <div className="image-details">
+      <div className="actions">
+        <ActionIconToggle
+          onClick={togglePreview}
+          state={preview}
+          states={{
+            [true]: <FiEye />,
+            [false]: <FiEdit />,
+          }}>
+        </ActionIconToggle>
+      </div>
 
+      <div className="content">
+        <Editable
+          className="h1"
+          placeholder="Image Title"
+          value={update.name}
+          onChange={name => setUpdate({ ...update, name })}
+          editing={editable}
+          >
+          <h1>{ update.name }</h1>
+        </Editable>
+
+        <Editable
+          className="story"
+          placeholder="Story or Description"
+          value={update.story}
+          onChange={story => setUpdate({ ...update, story })}
+          editing={editable}
+          >
+          <div className="story">
+            { md([update.story]) }
+          </div>
+        </Editable>
+      </div>
+
+
+
+
+{/*}
       {
         editable
         ? <LiveEdit
             className="h1"
             value={update.name}
             onChange={name => {
-              setIsDirty(true)
               setUpdate({ ...update, name })
             }}
             placeholder="Image Title"
@@ -34,7 +125,6 @@ export default function ImageDetails({ collectionId, item, updateItemAction }) {
             className="story"
             value={update.story}
             onChange={story => {
-              setIsDirty(true)
               setUpdate({ ...update, story })
             }}
             placeholder="Story or Description"
@@ -43,7 +133,7 @@ export default function ImageDetails({ collectionId, item, updateItemAction }) {
             { md([item.story]) }
           </div>
       }
-
+*/}
       {
         editable &&
         <button
@@ -52,7 +142,7 @@ export default function ImageDetails({ collectionId, item, updateItemAction }) {
             console.log('save changes', update, item, updateItemAction)
             updateItemAction({
               update,
-              onSuccess: () => setIsDirty(false),
+              // onSuccess: () => setIsDirty(false),
             })
           }}
           >
