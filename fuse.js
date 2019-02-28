ae2824a5f41ebae4244b977769cc9b6821d18f5a
Kevin Whitley <krwhitley@gmail.com>
2019-02-28 14:58:10 -0600

    removed preact refs

--- fuse.js
+++ fuse.js
@@ -26,9 +26,6 @@ console.log('description', pkg.description)
 
 const clientConfig = (isProduction, basePath = DEV_BUILD_PATH) => ({
   alias : {
-    // 'react' : 'preact-compat',
-    // 'react-dom' : 'preact-compat',
-    // 'mobx-react' : 'mobx-preact',
     'hooks': '~/client/hooks',
     'utils': '~/client/utils',
     'Common': '~/client/components/Common',
